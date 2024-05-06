e1b5a8946fa9d1c4d98ffc44120be86f0b39896e
Dimitrija Gjoshev <dimitrijagjoshev@Dimitrijas-MacBook-Pro.local>
2024-05-06 20:09:15 +0200

    More functions added

--- randomizer.py
+++ randomizer.py
@@ -1,16 +1,22 @@
 #!/usr/local/bin/python3
 
 import random
+import pprint
 
-print("""
+pprint.pprint("-"*70)
+print("""     
       If You want to see the list of children type in Lista 
       If You want to add dete to deca type Add
-      
+      If You want to delete a dete type Delete
+      If You want to create a List for Breakfast press Enter
       """)
+pprint.pprint("-"*70)
+
 deca = ("Alfi", "Bobby", "Slavco", "Bojan", "Slagjana", "Svetlana", "Stojanco")
 
 def show_deca(deca):
-    print(deca)
+    for broj,dete in enumerate(deca,start=1):
+        print(f"{broj:<2} | {dete:>5}")
 
 def randomize():
     #print(len(deca))
@@ -32,12 +38,12 @@ def randomize():
     print("-"*40)
     
 def main():
-    produkt=input("Pisi go produktot tuka ")
+    produkt=input("Vasiot izbor e ... (za da kreirate lista pritisnete Enter) ")
     if produkt == "Lista":
         show_deca(deca)
     else:
         randomize()
-    
+
 if __name__ == "__main__":
     main()
     
